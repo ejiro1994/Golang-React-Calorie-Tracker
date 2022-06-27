0937b5c67f3d1f2f2984cf772a1a42d398e26c48
Ejiro Akpoduado <ejiro.akpoduado@6point6.co.uk>
2022-06-27 18:23:26 +0100

    crud functions

--- routes/connection.go
+++ routes/connection.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client{
+	MongoDb := "mongodb://localhost:27017/caloriesdb"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := contex.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("Connected to Mongodb")
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OPenCollection(Client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("caloriesdb").Collection(collectionName)
+	return collection
+}
